a037ff3288cdf26b7180bddbef0ee58603a0c84e
Josh Cheek <josh.cheek@gmail.com>
2015-04-05 06:24:30 -0600

    Refactoring in Parse::Registry

--- lib/error_to_communicate/parse/registry.rb
+++ lib/error_to_communicate/parse/registry.rb
@@ -1,25 +1,23 @@
 module WhatWeveGotHereIsAnErrorToCommunicate
   class Parse
     class Registry
+      def initialize
+        @parsers ||= []
+      end
+
       def <<(parser)
-        parsers << parser
+        @parsers << parser
         self
       end
 
       def parse?(exception)
-        parsers.any? { |parser| parser.parse? exception }
+        @parsers.any? { |parser| parser.parse? exception }
       end
 
       def parse(exception)
-        parser = parsers.find { |parser| parser.parse? exception }
+        parser = @parsers.find { |parser| parser.parse? exception }
         return parser.parse exception if parser
         raise "NO PARSER FOUND FOR #{exception.inspect}"
-      end
-
-      private
-
-      def parsers
-        @parsers ||= []
       end
     end
   end
