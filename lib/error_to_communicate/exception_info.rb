07e8869080773a0d27434d2aef51c81bb56498e6
Josh Cheek <josh.cheek@gmail.com>
2015-04-05 07:33:58 -0600

    Syntactic tweaking in ExceptionInfo
    Just an attempt to make the structure and relationships a little bit clearer.

--- lib/error_to_communicate/exception_info.rb
+++ lib/error_to_communicate/exception_info.rb
@@ -1,39 +1,38 @@
 module WhatWeveGotHereIsAnErrorToCommunicate
   class ExceptionInfo
     attr_accessor :classname, :explanation, :backtrace
-
     def initialize(attributes)
-      self.classname             = attributes.fetch :classname
-      self.explanation           = attributes.fetch :explanation
-      self.backtrace             = attributes.fetch :backtrace
+      self.classname   = attributes.fetch :classname
+      self.explanation = attributes.fetch :explanation
+      self.backtrace   = attributes.fetch :backtrace
     end
+  end
 
-    class Location
-      attr_accessor :filepath, :linenum, :methodname, :pred, :succ
-      def initialize(attributes)
-        self.filepath   = attributes.fetch :filepath
-        self.linenum    = attributes.fetch :linenum
-        self.methodname = attributes.fetch :methodname
-        self.pred       = attributes.fetch :pred, nil
-        self.succ       = attributes.fetch :succ, nil
-      end
+  class ExceptionInfo::Location
+    attr_accessor :filepath, :linenum, :methodname, :pred, :succ
+    def initialize(attributes)
+      self.filepath   = attributes.fetch :filepath
+      self.linenum    = attributes.fetch :linenum
+      self.methodname = attributes.fetch :methodname
+      self.pred       = attributes.fetch :pred, nil
+      self.succ       = attributes.fetch :succ, nil
     end
+  end
 
-    class ArgumentError < ExceptionInfo
-      attr_accessor :num_expected, :num_received
-      def initialize(attributes)
-        self.num_expected = attributes.fetch :num_expected
-        self.num_received = attributes.fetch :num_received
-        super
-      end
+  class ExceptionInfo::ArgumentError < ExceptionInfo
+    attr_accessor :num_expected, :num_received
+    def initialize(attributes)
+      self.num_expected = attributes.fetch :num_expected
+      self.num_received = attributes.fetch :num_received
+      super
     end
+  end
 
-    class NoMethodError < ExceptionInfo
-      attr_accessor :undefined_method_name
-      def initialize(attributes)
-        self.undefined_method_name = attributes.fetch :undefined_method_name
-        super
-      end
+  class ExceptionInfo::NoMethodError < ExceptionInfo
+    attr_accessor :undefined_method_name
+    def initialize(attributes)
+      self.undefined_method_name = attributes.fetch :undefined_method_name
+      super
     end
   end
 end
