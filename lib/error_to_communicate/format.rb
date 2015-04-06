752c1bff4d112b79e135b1190ef40ead8f7b78bd
Josh Cheek <josh.cheek@gmail.com>
2015-04-06 03:21:19 -0600

    Pull some of the terminal helpers (eg named colours) out into a module

--- lib/error_to_communicate/format.rb
+++ lib/error_to_communicate/format.rb
@@ -1,39 +1,18 @@
 require 'coderay'
 require 'pathname'
+require 'error_to_communicate/format/terminal_helpers'
 
 module WhatWeveGotHereIsAnErrorToCommunicate
+  extend Format::TerminalHelpers
   def self.format(info)
-    # FIXME: extract this into some sort of object
-    separator = lambda do
-      ("="*70) << "\n"
-    end
+    cwd = Dir.pwd
 
-    color_path = lambda do |str|
-      "\e[38;5;36m#{str}\e[39m" # fg r:0, g:3, b:2 (out of 0..5)
-    end
-
-    color_linenum = lambda do |linenum|
-      "\e[34m#{linenum}\e[39m"
-    end
-
-    path_to_dir = lambda do |from, to|
-      begin
-        to.relative_path_from(from).dirname
-      rescue ArgumentError # rbx internal code
-        return to
-      end
-    end
-
-    display_class_and_message = lambda do
-      white   = "\e[38;5;255m"
-      bri_red = "\e[38;5;196m"
-      dim_red = "\e[38;5;124m"
-      none    = "\e[39m"
-      # FIXME:
-      # Something else should set this?
-      #   I'd say heuristic, but fact is that it needs formatting info.
-      #   Maybe initially, heuristic contains both extracted info and formatting info?
-      # Or maybe we want polymorphism at the formatter level?
+    # FIXME:
+    # Something else should set this?
+    #   I'd say heuristic, but fact is that it needs formatting info.
+    #   Maybe initially, heuristic contains both extracted info and formatting info?
+    # Or maybe we want polymorphism at the formatter level?
+    display_class_and_message = lambda do |info|
       if info.classname == 'ArgumentError'
         "#{white}#{info.classname} | "\
         "#{bri_red}#{info.explanation} "\
@@ -48,102 +27,42 @@ module WhatWeveGotHereIsAnErrorToCommunicate
       end
     end
 
-    bound_num = lambda do |attributes|
-      num = attributes.fetch :num
-      min = attributes.fetch :min
-      num < min ? min : num
-    end
-
-    remove_indentation = lambda do |code|
-      indentation = code.scan(/^\s*/).min_by(&:length)
-      code.gsub(/^#{indentation}/, "")
-    end
-
-    prefix_linenos_to = lambda do |code, start_linenum|
-      lines         = code.lines
-      max_linenum   = lines.count + start_linenum - 1 # 1 to translate to indexes
-      linenum_width = max_linenum.to_s.length + 1     # 1 for the colon
-      lines.zip(start_linenum..max_linenum)
-           .map { |line, num|
-             formatted_num = "#{num}:".ljust(linenum_width)
-             color_linenum.call(formatted_num) << " " << line
-           }.join("")
-    end
-
-    add_message_to = lambda do |code, offset, message|
-      lines = code.lines
-      lines[offset].chomp! << " " << message << "\n"
-      lines.join("")
-    end
-
-    highlight_text = lambda do |code, index, text|
-      lines = code.lines
-      return code unless lines[index]
-      lines[index].gsub!(text, "\e[7m#{text}\e[27m") # invert
-      lines.join("")
-    end
-
-    indent = lambda do |str, indentation_str|
-      str.gsub /^/, indentation_str
-    end
-
-    screaming_red = lambda do |text|
-      return "" if text.empty?
-      "\e[38;5;255;48;5;88m #{text} \e[39;49m" # bright white on medium red
-    end
-
-    underline = lambda do |str|
-      "\e[4m#{str}\e[24m"
-    end
-
-    color_filename = lambda do |str|
-      "\e[38;5;49;1m#{str}\e[39m" # fg r:0, g:5, b:3 (out of 0..5)
-    end
-
-    desaturate = lambda do |str|
-      nocolor = str.gsub(/\e\[[\d;]+?m/, "")
-      allgray = nocolor.gsub(/^(.*?)\n?$/, "\e[38;5;240m\\1\e[39m\n")
-      allgray
-    end
-
-    cwd = Dir.pwd
-
     display_location = lambda do |attributes|
       location       = attributes.fetch :location
       cwd            = Pathname.new attributes.fetch(:cwd)
       filepath       = Pathname.new location.filepath
       line_index     = location.linenum - 1
       highlight      = attributes.fetch :highlight, location.methodname
-      end_index      = bound_num.call min: 0, num: line_index+attributes.fetch(:context).end
-      start_index    = bound_num.call min: 0, num: line_index+attributes.fetch(:context).begin
+      end_index      = bound_num min: 0, num: line_index+attributes.fetch(:context).end
+      start_index    = bound_num min: 0, num: line_index+attributes.fetch(:context).begin
       message        = attributes.fetch :message, ''
       message_offset = line_index - start_index
 
       # first line gives the path
       path_line = ""
-      path_line << color_path.call("#{path_to_dir.call cwd, filepath}/")
-      path_line << color_filename.call(filepath.basename)
-      path_line << ":" << color_linenum.call(location.linenum)
+      path_line << color_path("#{path_to_dir cwd, filepath}/")
+      path_line << color_filename(filepath.basename)
+      path_line << ":" << color_linenum(location.linenum)
 
       # then display the code
       if filepath.exist?
         code = File.read(filepath).lines[start_index..end_index].join("")
-        code = remove_indentation.call code
-        code = CodeRay.encode          code, :ruby, :terminal
-        code = prefix_linenos_to.call  code, start_index.next
-        code = indent.call             code, "  "
-        code = add_message_to.call     code, message_offset, screaming_red.call(message)
-        code = highlight_text.call     code, message_offset, highlight
+        code = remove_indentation code
+        code = CodeRay.encode     code, :ruby, :terminal
+        code = prefix_linenos_to  code, start_index.next
+        code = indent             code, "  "
+        code = add_message_to     code, message_offset, screaming_red(message)
+        code = highlight_text     code, message_offset, highlight
       else
         code = "Can't find code\n"
       end
 
       # adjust for emphasization
       if attributes.fetch(:emphasisis) == :path
-        path_line = underline.call path_line
-        code = indent.call         code, "      "
-        code = desaturate.call     code
-        code = highlight_text.call code, message_offset, highlight # b/c desaturate really strips color
+        path_line = underline path_line
+        code = indent         code, "      "
+        code = desaturate     code
+        code = highlight_text code, message_offset, highlight # b/c desaturate really strips color
       end
 
       # all together
@@ -153,11 +72,11 @@ module WhatWeveGotHereIsAnErrorToCommunicate
 
     # Display the ArgumentError
     display = ""
-    display << separator.call
-    display << display_class_and_message.call << "\n"
+    display << separator
+    display << display_class_and_message.call(info) << "\n"
 
     # Display the Heuristic
-    display << separator.call
+    display << separator
 
     # FIXME: Some sort of polymorphism or normalization would be way better here, too
     # And, at the very least, not switching on classname, but some more abstract piece of info,
@@ -193,7 +112,7 @@ module WhatWeveGotHereIsAnErrorToCommunicate
     end
 
     # display the backtrace
-    display << separator.call
+    display << separator
     display << display_location.call(location:   info.backtrace[0],
                                      highlight:  info.backtrace[0].methodname,
                                      context:    0..0,
---
+++ lib/error_to_communicate/format/terminal_helpers.rb
@@ -0,0 +1,97 @@
+module WhatWeveGotHereIsAnErrorToCommunicate
+  module Format
+    module TerminalHelpers
+      def separator
+        ("="*70) << "\n"
+      end
+
+      def color_path(str)
+        "\e[38;5;36m#{str}\e[39m" # fg r:0, g:3, b:2 (out of 0..5)
+      end
+
+      def color_linenum(linenum)
+        "\e[34m#{linenum}\e[39m"
+      end
+
+      def path_to_dir(from, to)
+        to.relative_path_from(from).dirname
+      rescue ArgumentError
+        return to # eg rbx's core code
+      end
+
+      def white
+        "\e[38;5;255m"
+      end
+
+      def bri_red
+        "\e[38;5;196m"
+      end
+
+      def dim_red
+        "\e[38;5;124m"
+      end
+
+      def none
+        "\e[39m"
+      end
+
+      def bound_num(attributes)
+        num = attributes.fetch :num
+        min = attributes.fetch :min
+        num < min ? min : num
+      end
+
+      def remove_indentation(code)
+        indentation = code.scan(/^\s*/).min_by(&:length)
+        code.gsub(/^#{indentation}/, "")
+      end
+
+      def prefix_linenos_to(code, start_linenum)
+        lines         = code.lines
+        max_linenum   = lines.count + start_linenum - 1 # 1 to translate to indexes
+        linenum_width = max_linenum.to_s.length + 1     # 1 for the colon
+        lines.zip(start_linenum..max_linenum)
+             .map { |line, num|
+               formatted_num = "#{num}:".ljust(linenum_width)
+               color_linenum(formatted_num) << " " << line
+             }.join("")
+      end
+
+      def add_message_to(code, offset, message)
+        lines = code.lines
+        lines[offset].chomp! << " " << message << "\n"
+        lines.join("")
+      end
+
+      def highlight_text(code, index, text)
+        lines = code.lines
+        return code unless lines[index]
+        lines[index].gsub!(text, "\e[7m#{text}\e[27m") # invert
+        lines.join("")
+      end
+
+      def indent(str, indentation_str)
+        str.gsub /^/, indentation_str
+      end
+
+      def screaming_red(text)
+        return "" if text.empty?
+        "\e[38;5;255;48;5;88m #{text} \e[39;49m" # bright white on medium red
+      end
+
+      def underline(str)
+        "\e[4m#{str}\e[24m"
+      end
+
+      def color_filename(str)
+        "\e[38;5;49;1m#{str}\e[39m" # fg r:0, g:5, b:3 (out of 0..5)
+      end
+
+      def desaturate(str)
+        nocolor = str.gsub(/\e\[[\d;]+?m/, "")
+        allgray = nocolor.gsub(/^(.*?)\n?$/, "\e[38;5;240m\\1\e[39m\n")
+        allgray
+      end
+    end
+  end
+end
