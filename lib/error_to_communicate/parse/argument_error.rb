321e081f1d22a75d1a835fee1e80389f12f13f87
Josh Cheek <josh.cheek@gmail.com>
2015-04-06 01:58:56 -0600

    JRuby and Rbx pass parsing, but backtraces aren't including the called method
    Also, their backtraces are a bit fucked, getting `argument_error.rb/argument_error.rb',
    but should either be `argument_error.rb', or `proving_grounds/argument_error.rb'

--- lib/error_to_communicate/parse/argument_error.rb
+++ lib/error_to_communicate/parse/argument_error.rb
@@ -9,13 +9,19 @@ module WhatWeveGotHereIsAnErrorToCommunicate
       end
 
       def self.parse(exception)
+        case exception.message
+        when /(\d+) for (\d+)/
+          num_received, num_expected = $1.to_i, $2.to_i
+        when /given (\d+).*? expected (\d+)/
+          num_received, num_expected = $1.to_i, $2.to_i
+        end
         ExceptionInfo::ArgumentError.new(
           exception:    exception,
           classname:    exception.class.to_s,
           explanation:  'Wrong number of arguments',
           backtrace:    Backtrace.parse(exception),
-          num_expected: exception.message.scan(/\d+/)[-1].to_i,
-          num_received: exception.message.scan(/\d+/)[-2].to_i,
+          num_expected: num_expected,
+          num_received: num_received,
         )
       end
     end
