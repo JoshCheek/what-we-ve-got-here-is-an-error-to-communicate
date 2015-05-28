1a6a30d1ee32e666876d28737e4032f04d5b1fa8
Josh Cheek <josh.cheek@gmail.com>
2015-05-28 02:09:02 -0600

    Specs showing that you can require short or long forms of the name.
    eg:
      require "error_to_communicate/at_exit"
      require "what_weve_got_here_is_an_error_to_communicate"

--- lib/error_to_communicate.rb
+++ lib/error_to_communicate.rb
@@ -1,4 +1,5 @@
 require 'error_to_communicate/config'
 
-# for politeness :)
+# We will use the shorthand for convenience,
+# but include the pattern-based name, for politeness :)
 ErrorToCommunicate = WhatWeveGotHereIsAnErrorToCommunicate
---
+++ spec/acceptance/short_and_long_require_spec.rb
@@ -0,0 +1,29 @@
+require 'acceptance/spec_helper'
+
+RSpec.context 'Short and long require statements', acceptance: true do
+  it 'can require error_to_communicate' do
+    write_file 'require_shorthand.rb', <<-BODY
+      require 'error_to_communicate/at_exit'
+      ErrorToCommunicate
+      WhatWeveGotHereIsAnErrorToCommunicate
+    BODY
+
+    invocation = ruby 'load_shorthand.rb'
+    expect(invocation.stdout).to     eq ''
+    expect(invocation.stdout).to     eq ''
+    expect(invocation.exitstatus).to eq 1
+  end
+
+  it 'can require what_weve_got_here_is_an_error_to_communicate' do
+    write_file 'require_longhand.rb', <<-BODY
+      require 'what_weve_got_here_is_an_error_to_communicate'
+      ErrorToCommunicate
+      WhatWeveGotHereIsAnErrorToCommunicate
+    BODY
+
+    invocation = ruby 'load_longhand.rb'
+    expect(invocation.stdout).to     eq ''
+    expect(invocation.stdout).to     eq ''
+    expect(invocation.exitstatus).to eq 1
+  end
+end
