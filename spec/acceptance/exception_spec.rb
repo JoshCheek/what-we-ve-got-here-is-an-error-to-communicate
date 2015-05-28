bd74f0df4ff25dad1291c1fbcf5e4282bbdc926c
Josh Cheek <josh.cheek@gmail.com>
2015-05-28 01:18:20 -0600

    Make the acceptance tests a little stricter
    Too easy for them to pass right now, while being wrong.

--- spec/acceptance/exception_spec.rb
+++ spec/acceptance/exception_spec.rb
@@ -2,22 +2,20 @@ require 'acceptance/spec_helper'
 
 RSpec.context 'Exception', acceptance: true do
   it 'Prints heuristics' do
-    # Given a file with three lines in the backtrace that explodes on the third
     write_file 'exception.rb', <<-BODY
       require "what_weve_got_here_is_an_error_to_communicate"
       raise Exception, "mah message"
     BODY
 
     invocation = ruby 'exception.rb'
-    stderr = strip_color invocation.stderr
+    stderr     = strip_color invocation.stderr
 
     # sanity
     expect(invocation.stdout).to     eq ''
     expect(invocation.exitstatus).to eq 1
 
     # error: It prints the exception class and prints the message
-    expect(stderr).to include 'Exception'
-    expect(stderr).to include 'mah message'
+    expect(stderr).to match /Exception.*?mah message/
 
     # heuristic displays the line the exception was raised at, and some context
     expect(stderr).to include 'raise Exception, "mah message"'
--- spec/acceptance/no_methood_error_spec.rb
+++ spec/acceptance/no_methood_error_spec.rb
@@ -2,7 +2,7 @@ require 'acceptance/spec_helper'
 
 RSpec.describe 'NoMethodError', acceptance: true do
   it 'Prints heuristics' do
-    # Given a file that throws an error with two lines in the backtrace
+    # a file that throws an error with two lines in the backtrace
     write_file 'no_method_error.rb', <<-BODY
       require 'error_to_communicate/at_exit'
       module Test
@@ -32,19 +32,17 @@ RSpec.describe 'NoMethodError', acceptance: true do
       expect(stderr).to include "undefined method `backtrace4'"
 
     # heuristic:
-      # It displays a hlepful message next to the callsite
-      expect(stderr).to include 'backtrace4 is undefined'
+      # It displays a helpful message next to the callsite
+      expect(stderr).to match /Test\.backtrace4.*?backtrace4 is undefined/
 
       # It displays the line of the backtrace with the NoMethodError, and some context around it
       expect(stderr).to include 'def self.backtrace2'
       expect(stderr).to include '  Test.backtrace4'
       expect(stderr).to include 'end'
+      expect(stderr).to_not include 'error_to_communicate/at_exit'
 
     # backtrace: It displays each line of the backtrace and includes the code from that line
-      expect(stderr).to include 'no_method_error.rb:7'
-      expect(stderr).to include 'backtrace4'
-
-      expect(stderr).to include '14'
-      expect(stderr).to include 'backtrace2'
+      expect(stderr).to match /no_method_error\.rb:7\n.*?backtrace4/
+      expect(stderr).to match /no_method_error\.rb:14\n.*?Test.backtrace2/
   end
 end
